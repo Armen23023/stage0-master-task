5c6e56415679810d0b060318c9646e33ec868f34
Armen23023 <90245022+Armen23023@users.noreply.github.com>
2023-01-11 15:05:54 +0400

    Update NumberReverter.java

--- src/main/java/com/epam/langSyntax/NumberReverter.java
+++ src/main/java/com/epam/langSyntax/NumberReverter.java
@@ -2,7 +2,13 @@ package com.epam.langSyntax;
 
 public class NumberReverter {
 
-    public void revert(int number) {
+     public void revert(int number) {
+        String  r ="";
+        while(number!=0){
+            r = r + number%10;
+            number =  number/10;
+        }
+        System.out.println(r);
     }
 
 }
