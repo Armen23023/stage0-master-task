41735bcc33289cd2ce05d6c99a9eac017a4c6ab4
Armen23023 <90245022+Armen23023@users.noreply.github.com>
2023-01-11 18:18:04 +0400

    Update TriangleSidesValidator.java

--- src/main/java/com/epam/conditions/TriangleSidesValidator.java
+++ src/main/java/com/epam/conditions/TriangleSidesValidator.java
@@ -8,7 +8,7 @@ public class TriangleSidesValidator {
                 if (secondSide + thirdSide > firstSide) System.out.println("this is a valid triangle");
             }
         }
-        else System.out.println("it's not a triangle");
+        System.out.println("it's not a triangle");
     }
 
 }
