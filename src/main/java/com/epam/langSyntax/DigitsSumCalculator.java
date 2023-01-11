000618c31a1191dfa5d91964d7bac8007c2877c5
Armen23023 <90245022+Armen23023@users.noreply.github.com>
2023-01-11 15:06:22 +0400

    Update DigitsSumCalculator.java

--- src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
+++ src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
@@ -2,7 +2,13 @@ package com.epam.langSyntax;
 
 public class DigitsSumCalculator {
 
-    public void calculateSum(int number) {
+     public void calculateSum(int number) {
+        int  r = 0;
+        while(number!=0){
+            r = r + number%10;
+            number =  number/10;
+        }
+        System.out.println(r);
     }
 
 }
