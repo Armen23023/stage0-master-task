3344684914c27c59bf71e956e93aa2999b95d8ba
Armen23023 <90245022+Armen23023@users.noreply.github.com>
2023-01-11 16:32:29 +0400

    Update MultiplicationTable.java

--- src/main/java/com/epam/loops/MultiplicationTable.java
+++ src/main/java/com/epam/loops/MultiplicationTable.java
@@ -2,7 +2,21 @@ package com.epam.loops;
 
 public class MultiplicationTable {
 
-    public void printTable(int numberTableToPrint) {
+      public void printTable(int numberTableToPrint) {
+        if(numberTableToPrint<0) {
+            System.out.println("Please input a positive integer");
+            return;
+        }
+        System.out.println(numberTableToPrint + " * 0 = " + 0);
+        System.out.println(numberTableToPrint + " * 1 = " + numberTableToPrint);
+        System.out.println(numberTableToPrint + " * 2 = " + numberTableToPrint * 2);
+        System.out.println(numberTableToPrint + " * 3 = " + numberTableToPrint * 3);
+        System.out.println(numberTableToPrint + " * 4 = " + numberTableToPrint * 4);
+        System.out.println(numberTableToPrint + " * 5 = " + numberTableToPrint * 5);
+        System.out.println(numberTableToPrint + " * 6 = " + numberTableToPrint * 6);
+        System.out.println(numberTableToPrint + " * 7 = " + numberTableToPrint * 7);
+        System.out.println(numberTableToPrint + " * 8 = " + numberTableToPrint * 8);
+        System.out.println(numberTableToPrint + " * 9 = " + numberTableToPrint * 9);
     }
 
 }
